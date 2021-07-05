cdd833a07baf8d15c484ba8b2de0935c60225cbd
mufty <gubriansky@sernato.sk>
2021-07-05 10:13:15 +0200

    changing cloths and pay for them

--- client/main.js
+++ client/main.js
@@ -16,8 +16,7 @@ if (config.showBlips) {
         SetBlipSprite(blip, v.id);
         SetBlipScale(blip, 0.8);
         SetBlipAsShortRange(blip, true);
-        if (v.principal)
-            SetBlipColour(blip, config.blipColor);
+        SetBlipColour(blip, config.blipColor);
 
         BeginTextCommandSetBlipName("STRING");
         AddTextComponentString(v.name);
@@ -37,10 +36,57 @@ function nearShop() {
 
 setInterval(() => {
     if (nearShop()) {
+        //TODO add radial menu for changing and adding outfits
         MRP_CLIENT.displayHelpText(config.helpText1);
 
         if (IsControlJustPressed(1, 38)) {
+            console.log(`Open shop menu`);
             emit('mrp:clotheshop:show');
         }
     }
 }, 1);
+
+on('mrp:clotheshop:show', () => {
+    console.log('Opening cloth shop');
+
+    let config = {
+        ped: false,
+        headBlend: false,
+        faceFeatures: false,
+        headOverlays: false,
+        components: true,
+        props: true
+    };
+
+    let ped = PlayerPedId();
+    if (!ped) {
+        console.log("No PED for player");
+        return;
+    }
+
+    let appearanceBeforeChanging = exports["fivem-appearance"].getPedAppearance(ped);
+
+    let char = MRP_CLIENT.GetPlayerData();
+
+    if (char) {
+        exports["fivem-appearance"].startPlayerCustomization(appearance => {
+            if (appearance) {
+                if (char.stats.cash >= config.clothPrice) {
+                    emitNet('mrp:bankin:server:pay:cash', GetPlayerServerId(PlayerId()), config.clothPrice);
+                    console.log('Saved and paid for');
+                    console.log(JSON.stringify(appearance));
+                } else {
+                    emit('chat:addMessage', {
+                        template: '<div class="chat-message nonemergency">{0}</div>',
+                        args: [
+                            config.poor
+                        ]
+                    });
+                    exports["fivem-appearance"].setPedAppearance(ped, appearanceBeforeChanging);
+                }
+            } else {
+                console.log('Canceled cloth shopping');
+            }
+        }, config);
+    }
+});
